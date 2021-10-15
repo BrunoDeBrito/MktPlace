4cac4a35618907599f3ccd5845db92ff7cdf93c6
BrunoDeBrito <brunordebrito@gmail.com>
2021-10-14 22:37:41 -0300

    Iniciando mktplace com lrv6

---
+++ database/migrations/2021_06_09_032656_create_table_store.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStore extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up(){
+
+    Schema::create('stores', function (Blueprint $table) {
+
+      $table->bigIncrements('id');
+
+	  $table->unsignedBigInteger('user_id');
+
+      $table->string('name');
+      $table->string('description');
+      $table->string('mobile_phone');
+      $table->string('slug');
+
+	  $table->foreign('user_id')->references('id')->on('users');
+
+      $table->timestamps();
+
+    });
+  }
+
+  public function down(){
+
+    Schema::dropIfExists('stores');
+  }
+}
---
+++ database/migrations/2021_07_19_023056_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+
+			$table->unsignedBigInteger('store_id');
+
+			$table->string('name');
+			$table->string('descriptions');
+			$table->text('body');
+			$table->decimal('price', 10, 2);
+			$table->string('slug');
+
+			$table->foreign('store_id')->references('id')->on('stores');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
